1d98345435c9ab5fb08f859b331a6412948f1ad0
Ahmar Suhail <ahmar.suhail@gmail.com>
2019-05-30 11:57:05 -0700

    Subscriber changes

--- static/subscriber/src/main/java/edu/uci/ics/application/Application.java
+++ static/subscriber/src/main/java/edu/uci/ics/application/Application.java
@@ -1,8 +1,6 @@
 package edu.uci.ics.application;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Timestamp;
 
